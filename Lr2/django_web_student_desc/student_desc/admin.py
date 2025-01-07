9c10821fdff5fd35b7d2601709bb6c593aba198b
Алексей Оспельников <ospelnikov00@mail.ru>
2025-01-07 20:20:08 +0300

    upd: docs

--- Lr2/django_web_student_desc/student_desc/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from student_desc.models import ReadyTask, User, Homework, Mark, Subject
-
-admin.site.register(User)
-admin.site.register(Homework)
-admin.site.register(ReadyTask)
-admin.site.register(Mark)
-admin.site.register(Subject)
